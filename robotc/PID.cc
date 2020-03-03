a3d477d08ff15c2b8bc7ac7d134856a5b3cb6be3
Ravago Jones <ravagojones@icloud.com>
2020-03-03 15:16:59 -0800

    Make big repo

---
+++ robotc/PID.cc
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+struct PIDConstants {
+  float kP, kI, kD;
+};
+
+struct PIDState {
+  float prevError, integral, derivative;
+};
+
+int wait(int cycleLengthMs, int timer){
+  int startTime = time1[timer];
+  while(time1[timer] < cycleLengthMs){
+    delay(1);
+  }
+  int timeWaited = time1[timer] - startTime;
+  clearTimer(timer);
+  return timeWaited;
+}
+
+float iterate(float error, struct PIDConstants *consts, struct PIDState *state){
+  state->integral = state->integral + error;
+  state->derivative = error - state->prevError;
+  state->prevError = error;
+
+
+  float voltage = consts->kP * error + consts->kI * state->integral + consts->kD * state->derivative;
+
+  /*if(integral > 0 != error > 0){
+      integral = 0;
+  }*/
+
+  return voltage;
+}
